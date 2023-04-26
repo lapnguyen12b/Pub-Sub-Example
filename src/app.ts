1a8d26f4115a9f6513df80a6c750bacfb1b44490
lapnv <lapnguyen12b@gmail.com>
2023-04-26 10:19:51 +0700

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { ConcretePublisher } from "./publisher";
 import { Subscriber, ConcreteSubscriber } from "./subscriber";
 
-// Sử dụng các class trên để tạo ra một đối tượng Publisher và Subscriber
 const publisher = new ConcretePublisher();
 const subscriberA: Subscriber = new ConcreteSubscriber();
 const subscriberB: Subscriber = new ConcreteSubscriber();
@@ -13,10 +12,10 @@ publisher.subscribe(subscriberB);
 publisher.subscribe(subscriberC);
 
 // Push notify 1 to all Subscriber
-publisher.publish("notify 1 !");
+publisher.publish("Notification 1 !");
 // UnSubscribe
 publisher.unsubscribe(subscriberA);
 publisher.unsubscribe(subscriberB);
 console.log('--- After UnSubcribe A, B ---')
 // Push notify 2 to all Subscriber
-publisher.publish("notify 2 !");
+publisher.publish("Notification 2 !");
--- src/subscriber.ts
+++ src/subscriber.ts
@@ -4,6 +4,6 @@ export interface Subscriber {
 
 export class ConcreteSubscriber implements Subscriber {
   update(data: any): void {
-    console.log(`Received data: ${data}`);
+    console.log(`Send message: ${data}`);
   }
 }
